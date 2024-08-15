39bbefda13a3d5ef9b1529b8db9e46c8ee409a5f
Ujjwal patidar <147056624+Ujjwal0p@users.noreply.github.com>
2024-08-16 01:41:41 +0530

    add files

---
+++ Strings.java
@@ -0,0 +1,17 @@
+public class Strings {
+    public static void main(String[] args) {
+        String str="Java Programming";
+        String str1=new String();
+        str1="Good Morning";
+        System.out.println(str);
+        System.out.println(str1);
+        System.out.println(str + " " +str1);
+        int len=str.length();
+        System.out.println(len);
+        String concatenate=str.concat(str1);
+        System.out.println(concatenate);
+
+
+    }   
+    
+} 
